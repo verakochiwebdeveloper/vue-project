14420b7fc01dada3be69d5e3c767e15b7abc4600
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-07-29 15:51:06 +0300

    project start

---
+++ coffe-shop/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    Hello Vue.JS world
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
