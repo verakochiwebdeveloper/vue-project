36afefff251bbb40ae09d373a69de33d88c15c87
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-08-17 14:36:09 +0300

    validate to vue

---
+++ coffe-shop/src/validators/minlength.js
@@ -0,0 +1,3 @@
+export const mixlength = (value)=> {
+    return value.length > 5
+  }
--- coffe-shop/src/views/ContactUsViewe.vue
+++ coffe-shop/src/views/ContactUsViewe.vue
@@ -23,40 +23,64 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.name" type="text" class="form-control" id="name-input" />
+                  <input
+                    v-model="v$.name.$model"
+                    type="text"
+                    class="form-control"
+                    id="name-input"
+                  />
                 </div>
+                <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{error.$name}}
+                  </span>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.email" type="email" class="form-control" id="email-input" />
+                  <input
+                    v-model="v$.email.$model"
+                    type="email"
+                    class="form-control"
+                    id="email-input"
+                  />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{error.$email}}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-stsrt">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.phone" type="tel" class="form-control" id="phone-input" />
+                  <input
+                    v-model="v$.phone.$model"
+                    type="tel"
+                    class="form-control"
+                    id="phone-input"
+                  />
+                  <span v-for="error in v$.phone.$errors" :key="error.$uid">
+                    {{error.phone}}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -64,13 +88,16 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                  v-model="form.message"
+                    v-model="v$.message.$modal"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{error.$massage}}
+                  </span>
                 </div>
               </div>
 
@@ -88,23 +115,41 @@
 </template>
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, maxLength } from '@vuelidate/validators'
+import { helpers } from '@vuelidate/validators'
+import mixlength from "../validators/minmilength"
+
 
 export default {
+  setup () {
+    return { v$: useVuelidate() }
+  },
   data() {
     return {
-      form: {
         name: "",
         email: "",
         phone: "",
         message: "",
-      },
-    };
+
+    }
   },
   components: { NavBarComponent },
+   validations () {
+    return {
+       name: { required },
+        email: { required, email },
+        phone: { required },
+        message: { required, maxLength: maxLength(20), mixlength: helpers.withMessage('this value min 5', mixlength )},
+      }
+    },
+  
   methods: {
-    submit() {
-        console.log(this.form);
-        
+   async submit() {
+        const isFormCorrect = await this.v$.$validate()
+      if (!isFormCorrect) return
+      
+
     }
   }
 };
